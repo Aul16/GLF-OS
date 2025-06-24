4e669eaf0782617f40df399b6c7159b2a5be8a40
Aul16 <4ul_16@pm.me>
2025-06-24 15:44:40 +0200

    intel workaround following the doc of niri

--- modules/default/environments/niri.nix
+++ modules/default/environments/niri.nix
@@ -25,10 +25,7 @@
     hardware.opengl = {
       enable = true;
       extraPackages = with pkgs; [
-        ... # your Open GL, Vulkan and VAAPI drivers
         vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
-        # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
-        # intel-media-sdk   # for older GPUs
       ];
     };
 
