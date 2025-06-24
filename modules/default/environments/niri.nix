c2ff97d075d66b3f83e58d8f4548458ebd20f172
Aul16 <4ul_16@pm.me>
2025-06-24 15:43:56 +0200

    intel workaround following the doc of niri

--- modules/default/environments/niri.nix
+++ modules/default/environments/niri.nix
@@ -20,6 +20,18 @@
       desktopManager.plasma6.enable = true;
     };
     hardware.bluetooth.enable = true;
+
+    # Workaround for intel according to https://nixos.wiki/wiki/Intel_Graphics and https://github.com/YaLTeR/niri/wiki/Getting-Started
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        ... # your Open GL, Vulkan and VAAPI drivers
+        vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
+        # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
+        # intel-media-sdk   # for older GPUs
+      ];
+    };
+
     programs.niri.enable = true;
     documentation.nixos.enable = false;
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
